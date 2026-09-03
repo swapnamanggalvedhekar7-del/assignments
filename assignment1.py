c6c4b52538672a25705888b6b94f667c6fca19b2
swapnamanggalvedhekar7-del <swapnamanggalvedhekar7@gmail.com>
2026-09-03 23:42:07 +0530

    assignment1.py updated with marker

--- assignment1.py
+++ assignment1.py
@@ -8,12 +8,14 @@ from playwright.sync_api import Playwright, Page, expect
 # playwright test runner includes browser management, reporting, assertions
 
 #@pytest.mark.only---this will focus on test case
+@pytest.mark.smoke
 def test_firstsmoketest(page:Page):
     page.goto("https://eventhub.rahulshettyacademy.com/")
     expect(page.locator(".text-xl")).to_have_text("Sign in to EventHub")
     expect(page.get_by_placeholder("you@email.com")).to_be_visible()
     expect(page.get_by_role("button", name="Sign In")).to_be_visible()
 
+@pytest.mark.smoke
 def test_secondsmoketest(page:Page):
     page.goto("https://eventhub.rahulshettyacademy.com/")
     expect(page.locator(".text-xl")).to_have_text("Sign in to EventHub")
