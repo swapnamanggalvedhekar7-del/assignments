c0394b7841b8dbbb9f7804cb876fab2001cd2a62
swapnamanggalvedhekar7-del <swapnamanggalvedhekar7@gmail.com>
2026-09-07 23:45:48 +0530

    updated assignment3.py, function name

--- assignment3.py
+++ assignment3.py
@@ -3,7 +3,7 @@ import time
 from playwright.sync_api import Playwright, expect
 
 
-def test_open_browser(playwright: Playwright):
+def test_eventhub_e2e(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page =  context.new_page()
